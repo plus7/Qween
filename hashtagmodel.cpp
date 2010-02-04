606c21814d9a8fdb941c32349794ef4182c2535f
plus7 <ahya@ahya-magnate.is.s.u-tokyo.ac.jp>
2010-02-04 15:47:24 +0900

    HashTagModel

---
+++ hashtagmodel.cpp
@@ -0,0 +1,5 @@
+#include "hashtagmodel.h"
+
+HashTagModel::HashTagModel()
+{
+}
---
+++ hashtagmodel.h
@@ -0,0 +1,13 @@
+#ifndef HASHTAGMODEL_H
+#define HASHTAGMODEL_H
+
+#include <QAbstractItemModel>
+
+class HashTagModel : public QAbstractItemModel
+{
+    Q_OBJECT
+public:
+    HashTagModel();
+};
+
+#endif // HASHTAGMODEL_H
