59fc26aaffeaf0be28a52404f6c3b05956273137
NOSE Takafumi <ahya365@gmail.com>
2010-03-20 12:13:06 +0900

    update changelog maker

--- make_changelog.py
+++ make_changelog.py
@@ -5,15 +5,15 @@ import os
 from xml.dom.minidom import parse
 
 def getText(nodelist):
-    rc = ""
+    rc = u""
     for node in nodelist:
         if node.nodeType == node.TEXT_NODE:
             rc = rc + node.data
     return rc
 
 def print_li(li, level):
-    indent = "\t" * (level+1)
-    print indent + "* " + getText(li.childNodes).replace("\n","")
+    indent = u"\t" * (level+1)
+    print (indent + u"* " + getText(li.childNodes).replace(u"\n",u"")).encode("utf_8")
     for child in li.childNodes:
         if child.nodeType == child.ELEMENT_NODE and child.tagName == "ul":
             print_ul(child, level+1)
@@ -31,7 +31,7 @@ def print_entry(entry):
     date = getText(entry.getElementsByTagName("date")[0].childNodes)
     ver = getText(entry.getElementsByTagName("version")[0].childNodes)
     content = entry.getElementsByTagName("content")[0]
-    print date + "  " + ver
+    print date + u"  " + ver
     print 
     print_content(content)
     print
